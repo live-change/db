893f3e7c236c6823809499c7343a5d7df5175343
Michał Łaszczewski <michal@laszczewski.com>
2021-04-05 13:59:11 +0200

    startConfig fix, profiling

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const OpLogger = require("./lib/OpLogger.js")
 const Index = require("./lib/Index.js")
 const AtomicWriter = require("./lib/AtomicWriter.js")
 const Database = require("./lib/Database.js")
+const profileLog = require('./lib/profileLog.js')
 
 module.exports = {
-  Log, Table, OpLogger, Index, AtomicWriter, Database
+  Log, Table, OpLogger, Index, AtomicWriter, Database, profileLog
 }
